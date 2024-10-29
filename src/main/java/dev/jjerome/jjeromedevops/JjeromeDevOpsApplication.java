bb1f21168485a2c0d4e976330d117557f6c1f546
JJerome-NM <mykhailo.khalabarchuk.21@pnu.edu.ua>
2024-10-29 14:45:31 +0200

    [JJerome] Initial commit

---
+++ src/main/java/dev/jjerome/jjeromedevops/JjeromeDevOpsApplication.java
@@ -0,0 +1,13 @@
+package dev.jjerome.jjeromedevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjeromeDevOpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JjeromeDevOpsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jjerome/jjeromedevops/JjeromeDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jjerome.jjeromedevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjeromeDevOpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
